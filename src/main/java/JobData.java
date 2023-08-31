1ece20a56a0190a2c3a00c831d0689d514a701fc
Tammy <tammylwalls@gmail.com>
2023-08-30 21:33:09 -0500

    Got the find by value method working.  Tried out the HashSet, but found it was not compatible with the printJobs method.  Checked the row contained value && ! ArrayList.

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -89,13 +86,23 @@ public class JobData {
      * @param value The search term to look for
      * @return      List of all jobs with at least one field containing the value
      */
-    public static ArrayList<HashMap<String, String>> findByValue(String value) {
+    public static ArrayList<HashMap<String,String>> findByValue(String value) {
 
         // load data, if not already loaded
         loadData();
 
-        // TODO - implement this method
-        return null;
+       ArrayList<HashMap<String, String>> jobs = new ArrayList<>();
+
+
+        for (HashMap<String, String> row : allJobs) {
+
+
+            if (row.containsValue(value)&& !jobs.contains(row)) {
+                jobs.add(row);
+            }
+        }
+
+        return jobs;
     }
 
     /**
